5ada21bbead86ca725a698a58004dad89f378929
Daniela Maica <danielamaica43@gmail.com>
2025-09-08 10:43:55 +0000

    arreglo final

--- src/js/components/Card.jsx
+++ src/js/components/Card.jsx
@@ -6,7 +6,7 @@ const Card = (props) => {
     return (
         <div className="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-3 p-0 d-flex justify-content-center">
             <ACard className="p-0 my-3 mx-3" style={{ width: '200%' }} >
-                <ACard.Img src="../src/img/image.png" className="card-img-top" alt="..." />
+                <ACard.Img src="../src/img/image.png" className="card-img-top" alt="imagen 500x325 gris" />
                 <ACard.Body className="text-center">
                     <ACard.Title>Card Title</ACard.Title>
                     <ACard.Text> {props.text} </ACard.Text>
